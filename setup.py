7f3ad71850255d9f993c23568442be5c7db9dc1c
djdt <10266332+djdt@users.noreply.github.com>
2023-03-24 13:20:29 +1100

    fix setup requires

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 setup(
     name="DGet",
     version="0.1",
@@ -8,9 +7,6 @@ setup(
     packages=["masscalc"],
     license="LGPL",
     author="djdt",
-    install_requires=[
-        "numpy",
-    ],
-    requires=["numpy>=1.22", "masscalc"],
+    install_requires=["numpy>=1.22", "masscalc"],
     entry_points={"console_scripts": ["dget=dget.__main__:main"]},
 )
