82d547a8009d97638fb2758966fb050fba4a5401
djdt <10266332+djdt@users.noreply.github.com>
2023-03-26 19:24:29 +1100

    add long description to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open("README.md") as fp:
+    long_description = fp.read()
+
 setup(
     name="DGet",
     version="0.2",
-    description="Deuteration calculator for ToF-MS data.",
+    description="Calculates compound deuteration from ToF-MS data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["dget"],
-    license="LGPL",
+    license="GPL3",
     author="djdt",
+    url="https://github.com/djdt/dget",
     install_requires=["numpy>=1.22", "molmass"],
     entry_points={"console_scripts": ["dget=dget.__main__:main"]},
 )
