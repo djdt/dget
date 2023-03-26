0890b08695d951bd2323ff94c0c92d5bb00b5923
djdt <10266332+djdt@users.noreply.github.com>
2023-03-26 12:21:04 +1100

    desc and molmass

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="DGet",
-    version="0.1",
-    description="Cacluator for isotopic abundances.",
+    version="0.2",
+    description="Deuteration calculator for ToF-MS data.",
     packages=["dget"],
     license="LGPL",
     author="djdt",
-    install_requires=["numpy>=1.22"],
+    install_requires=["numpy>=1.22", "molmass"],
     entry_points={"console_scripts": ["dget=dget.__main__:main"]},
 )
