1915238611ce23e5470e5d5e0579534c2735f032
djdt <10266332+djdt@users.noreply.github.com>
2023-03-27 17:12:32 +1100

    try to auto gen

--- web/web.py
+++ web/web.py
@@ -1,20 +1,69 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 from pyodide.ffi import create_proxy
-from pathlib import Path
+from pyscript import Element, display, js
+
 from dget import DGet
 
+delimiters = {"Comma": ",", "Semicolon": ";", "Tab": "\t", "Space": " "}
+
+
 def parse_inputs() -> dict | None:
-    inputs = {"formula": Element('formula').value}
-    inputs['delimiter'] = js.document.querySelector("[name='delimiter']:checked").value
-    if inputs['delimiter'] == "tab":
-        inputs['delimiter'] = "\t"
-    inputs['skiprows'] = int(Element('skiprows').value or 0)
-    masscol = int(Element('masscol').value or 1) - 1
-    signalcol = int(Element('signalcol').value or 1) - 1
-    inputs['usecols'] = (masscol, signalcol)
+    inputs = {"formula": Element("formula").value}
+
+    delimiter = js.document.querySelector("[name='delimiter']:checked").value
+    inputs["delimiter"] = delimiters[delimiter]
+
+    inputs["skiprows"] = int(Element("skiprows").value or 0)
+
+    masscol = int(Element("masscol").value or 1) - 1
+    signalcol = int(Element("signalcol").value or 1) - 1
+    inputs["usecols"] = (masscol, signalcol)
+
     return inputs
 
+
+def guess_inputs(path: Path) -> None:
+    with path.open("r") as fp:
+        lines = fp.readlines(1024)
+
+    print(lines)
+    for key, val in delimiters.items():
+        if all(val in line for line in lines):
+            print("setting delimiter ", key, val)
+            Element("delimiter").value = key
+            break
+
+    skiprows = 0
+    for line in lines:
+        try:
+            float(line.split(val)[-1])
+            break
+        except ValueError:
+            pass
+        skiprows += 1
+    print('setting skiprows', skiprows)
+    Element("skiprows").write(skiprows)
+    print('setting skiprows', skiprows)
+    Element("skiprows").value = skiprows
+    print('setting skiprows', skiprows)
+    Element("skiprows").write(str(skiprows))
+
+    header = lines[skiprows - 1].split(val)
+    for i, text in enumerate(header):
+        if any(x in text.lower() for x in ["mass", "m/z"]):
+            print('setting masscol', i)
+            Element("masscol").value = i
+        if any(x in text.lower() for x in ["signal", "intensity", "counts"]):
+            print('setting signalcol', i)
+            Element("signalcol").value = i
+
+
 def run():
+    # Clear the terminal
+    js.document.getElementsByClass("py-termial")[0].innerHTML = ""
+
     inputs = parse_inputs()
     path = Path("data.csv")
     if inputs["formula"] == "":
@@ -26,19 +75,18 @@ def run():
     if not path.exists():
         print("Error: no file uploaded.")
         return
-    
+
     try:
         dget = DGet(inputs.pop("formula"), path, loadtxt_kws=inputs)
     except Exception as e:
-        print(e)
+        print("Error:", e)
         return
 
-    fig, ax = plt.subplots(1, 1, figsize=(5,3))
+    fig, ax = plt.subplots(1, 1, figsize=(5, 3))
     dget.plot_predicted_spectra(ax)
     fig.tight_layout()
     display(fig, target="figure", append=False)
-    # Clear the terminal
-    js.document.getElementsByClass("py-termial")[0].innerHTML = ""
+
     print(f"Formula          : {dget.formula}")
     print(f"M/Z              : {dget.formula.mz}")
     print(f"Monoisotopic M/Z : {dget.formula.isotope.mz}")
@@ -48,12 +96,14 @@ def run():
     for i, p in enumerate(dget.deuteration_probabilites):
         print(f"D{i:<2}              : {p:.4f}")
 
+
 async def get_ms_data(event):
     file = event.target.files.item(0)
     text = await file.text()
-    path = Path("data.csv")
     with Path("data.csv").open("w") as fp:
         fp.write(text)
+    guess_inputs(Path("data.csv"))
+
 
 proxy = create_proxy(get_ms_data)
 js.document.getElementById("file").addEventListener("change", proxy, False)
