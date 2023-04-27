e1f38bc5601a15266010ed4a9696c60fa61e233c
djdt <10266332+djdt@users.noreply.github.com>
2023-04-27 15:14:27 +1000

    add version

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,14 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from dget import __version__
 
 project = "DGet"
 copyright = "2023, djdt"
 author = "djdt"
 release = "2023"
+version = __version__
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -29,7 +32,4 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "collapse_navigation": False,
-}
 html_static_path = ["_static"]
