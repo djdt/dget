8b750fc03b69d8be8a6d4282d0ac16ac7628cdf9
djdt <10266332+djdt@users.noreply.github.com>
2023-07-09 15:36:04 +1000

    added a report

--- web/main.py
+++ web/main.py
@@ -1,13 +1,13 @@
+import datetime
 from io import TextIOWrapper
 
-from io import StringIO
-
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, abort, json, render_template, request
 
-from dget import DGet
+from dget import DGet, __version__
 
 app = Flask(__name__)
+
 
 adducts = [
     "Auto",
@@ -23,17 +23,44 @@ adducts = [
 ]
 delimiters = {"Comma": ",", "Semicolon": ";", "Tab": "\t", "Space": " "}
 
-ms_data = None
+
+@app.errorhandler(500)
+def handle_error(error):
+    return {"error": str(error)}, 500
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", adducts=adducts, delimiters=delimiters)
+    return render_template(
+        "index.html", version=__version__, adducts=adducts, delimiters=delimiters
+    )
 
 
-@app.post("/guess_inputs")
+@app.post("/report")
+def report():
+    result = json.loads(request.form["result"])
+    return render_template(
+        "report.html",
+        version=__version__,
+        date=datetime.date.today(),
+        formula=result["formula"],
+        adduct=result["adduct"],
+        mz=result["m/z"],
+        adduct_mz=result["adduct m/z"],
+        options={"a": 1},
+        deuteration=result["deuteration"] * 100.0,
+        ratios={
+            s: p * 100.0 for s, p in zip(result["states"], result["probabilities"])
+        },
+        image=request.form["img"],
+    )
+
+
+@app.post("/api/guess_inputs")
 def guess_inputs():
     lines = request.form.getlist("lines[]")
+    if len(lines) == 0:
+        raise ValueError("Text not uploaded.")
     result = {}
 
     # find delimiter
@@ -64,13 +91,21 @@ def guess_inputs():
     return result
 
 
-@app.post("/calculate")
+@app.post("/api/calculate")
 def calculate():
     formula = request.form["formula"]
+    if len(formula) == 0 or formula.count("D") == 0:
+        abort(500, description="Missing or invalid formula.")
+
+    if "data" not in request.files:
+        abort(500, description="Missing MS data upload.")
+
     file = TextIOWrapper(request.files["data"])
+
     adduct = request.form["adduct"]
     if adduct == "Auto":
         adduct = None
+
     loadtxt_kws = {
         "delimiter": request.form["delimiter"],
         "skiprows": request.form.get("skiprows", type=int),
@@ -79,41 +114,54 @@ def calculate():
             request.form.get("signalcol", type=int) - 1,
         ),
     }
-    dget = DGet(
-        deuterated_formula=formula, tofdata=file, adduct=adduct, loadtxt_kws=loadtxt_kws
-    )
-    if adduct is None:
-        adduct, diff = dget.guess_adduct_from_base_peak()
-        dget.adduct = adduct
-    if request.form.get("align") == "true":
-        offset = dget.align_tof_with_spectra()
-    if request.form.get("baseline") == "true":
-        baseline = dget.subtract_baseline()
+    try:
+        dget = DGet(
+            deuterated_formula=formula,
+            tofdata=file,
+            adduct=adduct,
+            loadtxt_kws=loadtxt_kws,
+        )
+        if adduct is None:
+            adduct, diff = dget.guess_adduct_from_base_peak()
+            dget.adduct = adduct
+        if request.form.get("align") == "true":
+            offset = dget.align_tof_with_spectra()
+        if request.form.get("baseline") == "true":
+            baseline = dget.subtract_baseline()
+    except Exception as e:
+        abort(500, description=f"Processing failed with error: {e}")
 
-    dx = dget.targets
-    dy = np.convolve(dget.deuteration_probabilites, dget.psf, mode="full")
-    dy /= dy.max()
+    try:
+        dx = dget.targets
+        dy = np.convolve(dget.deuteration_probabilites, dget.psf, mode="full")
+        dy /= dy.max()
 
-    start, end = np.searchsorted(dget.x, (dget.targets[0], dget.targets[-1]))
-    x = dget.x[start:end]
-    y = dget.y[start:end]
-    y /= y.max()
+        start, end = np.searchsorted(dget.x, (dget.targets[0], dget.targets[-1]))
+        x = dget.x[start:end]
+        y = dget.y[start:end]
+        y /= y.max()
 
-    probabilities = dget.deuteration_probabilites[dget.deuteration_states]
-    probabilities /= probabilities.sum()
+        probabilities = dget.deuteration_probabilites[dget.deuteration_states]
+        probabilities /= probabilities.sum()
+    except Exception as e:
+        abort(500, description=f"Processing failed with error: {e}")
 
     return {
-        "x": x.tolist(),
-        "y": y.tolist(),
-        "dx": dx.tolist(),
-        "dy": dy.tolist(),
-        "formula": dget.base_name,
-        "adduct": dget.adduct.adduct,
-        "m/z": dget.adduct.base.isotope.mz,
-        "adduct m/z": dget.formula.isotope.mz,
-        "deuteration": dget.deuteration,
-        "states": dget.deuteration_states.tolist(),
-        "probabilities": probabilities.tolist(),
+        "chart": {
+            "x": x.tolist(),
+            "y": y.tolist(),
+            "dx": dx.tolist(),
+            "dy": dy.tolist(),
+        },
+        "compound": {
+            "formula": dget.base_name,
+            "adduct": dget.adduct.adduct,
+            "m/z": dget.adduct.base.isotope.mz,
+            "adduct m/z": dget.formula.isotope.mz,
+            "deuteration": dget.deuteration,
+            "states": dget.deuteration_states.tolist(),
+            "probabilities": probabilities.tolist(),
+        },
     }
 
 
--- web/static/script.js
+++ web/static/script.js
@@ -1,16 +1,126 @@
+function isComplete() {
+    var formula = $("#formula").val();
+    if (formula.length == 0 || !formula.includes("D")) {
+        return false;
+    }
+    // if ($("#adduct").val().length == 0) {
+    //     return false;
+    // }
+    if ($("#upload")[0].files.length == 0) {
+        return false;
+    }
+    return true;
+}
 
-// function uploadHandler(e) {
-//     file = e.target.files.item(0);
-//     reader = new FileReader();
-//     reader.addEventListener("load", guessUploadInputs);
-//     if (file) {
-//         reader.readAsText(file);
-//     }
-// };
+function completeChanged() {
+    var complete = isComplete();
+    $("#calculate").prop("disabled", !complete);
+}
 
-// function guessUploadInputs() {
-//     lines = reader.result.split("\n");
-//     for delim in [",", ";", "\t", " "] {
-//         if (delim)
-//     }
-// }
+var result;
+function clearResult() {
+    result = null;
+    $("button#report").prop("disabled", true);
+}
+function storeResult(res) {
+    result = res;
+    $("button#report").prop("disabled", false);
+}
+
+function getFormData() {
+    fd = new FormData();
+    fd.append("data", $("#upload")[0].files[0]);
+    fd.append("formula", $("#formula").val());
+    fd.append("adduct", $("#adduct").val());
+    fd.append("delimiter", $("#delimiter").val());
+    fd.append("skiprows", $("#skiprows").val());
+    fd.append("masscol", $("#masscol").val());
+    fd.append("signalcol", $("#signalcol").val());
+    fd.append("align", $("#align").prop("checked"));
+    fd.append("baseline", $("#baseline").prop("checked"));
+    return fd;
+}
+
+function updateOutputs(result) {
+    html = `<p>Formula: ${result["formula"]}<br>
+        Adduct: ${result["adduct"]}<br>
+        m/z: ${result["m/z"].toFixed(4)}<br>
+        Adduct m/z: ${result["adduct m/z"].toFixed(4)}<br>
+        %Deuteration: ${(result["deuteration"] * 100.0).toFixed(2)} %<br>
+        <br>
+        Deuteration Ratio Spectra<br>
+        `
+    html += result["states"].map((s, i) => {
+        return `D${s}: ${(result["probabilities"][i] * 100).toFixed(2)} %`
+    }).join("<br>");
+    html += "</p>";
+    $("#output").html(html);
+}
+
+function createChart(canvas) {
+    chart = new Chart(canvas, {
+        type: 'scatter',
+        data: {
+            datasets: [
+            {
+                label: "MS Data",
+                borderColor: "black",
+                showLine: true,
+                pointRadius: 0,
+                order: 2,
+            },
+            {
+                label: "Deconvoled Spectra",
+                backgroundColor: "#db5461",
+                pointRadius: 5,
+                order: 1,
+            },
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+            normalised: true,
+            responsive: true,
+            resizeDelay: 0,
+            spanGaps: true,
+            borderJoinStyle: "round",
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: "m/z",
+                    }
+                },
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function updateChart(mx, my, dx, dy) {
+    chart.data.datasets[0].data = mx.map((x, i) => {
+        return {x: x, y: my[i]}
+    });
+    chart.update("none");
+    chart.data.datasets[1].data = dx.map((x, i) => {
+        return {x: x, y: dy[i]}
+    });
+    chart.update();
+}
+
+function chartAsImage(x, y) {
+    w = chart.width;
+    h = chart.height;
+    chart.resize(x, y)
+    img = chart.toBase64Image()
+    chart.resize(w, h);
+    return img;
+}
+
+window.addEventListener("load", function() {
+    createChart(document.getElementById("plot"));
+    completeChanged();
+    clearResult();
+});
