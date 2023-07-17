6dc67ae2c5710fb3084fb63141066c0603d737ee
djdt <10266332+djdt@users.noreply.github.com>
2023-07-17 14:28:28 +1000

    prevent reporting same col as mass and signal

--- dget/io/text.py
+++ dget/io/text.py
@@ -3,6 +3,9 @@ from typing import List
 
 delimiters = [";", ",", "\t", " "]
 
+mass_hints = ["mass", "m/z", "thompsons"]
+signal_hints = ["signal", "intensity", "counts", "cps"]
+
 
 def guess_loadtxt_kws(header: List[str], loadtxt_kws: dict | None = None) -> dict:
     if loadtxt_kws is None:
@@ -29,10 +32,11 @@ def guess_loadtxt_kws(header: List[str], loadtxt_kws: dict | None = None) -> dic
             loadtxt_kws["delimiter"]
         )
         for i, text in enumerate(last_text_line):
-            masscol, signalcol = -1, -1
-            if any(x in text.lower() for x in ["mass", "m/z"]):
+            masscol, signalcol = None, None
+            if masscol is None and any(x in text.lower() for x in mass_hints):
                 masscol = i
-            if any(x in text.lower() for x in ["signal", "intensity", "counts"]):
+                continue
+            if any(x in text.lower() for x in signal_hints):
                 signalcol = i
         if masscol is not None and signalcol is not None:
             loadtxt_kws["usecols"] = (masscol, signalcol)
