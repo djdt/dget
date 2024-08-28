7a626023b4cddf7c1aaf653564e1dd0d72c85f1a
djdt <10266332+djdt@users.noreply.github.com>
2024-08-28 17:00:39 +1000

    start of chart

--- dget/gui/mainwindow.py
+++ dget/gui/mainwindow.py
@@ -2,6 +2,8 @@ import logging
 import sys
 from types import TracebackType
 
+import numpy as np
+import pyqtgraph
 from PySide6 import QtCore, QtGui, QtWidgets
 from spcal.gui.log import LoggingDialog
 
@@ -78,6 +80,84 @@ class DGetResults(QtWidgets.QDockWidget):
         self.setWidget(self.text)
 
 
+class LimitBoundViewBox(pyqtgraph.ViewBox):
+    """Viewbox that autoRanges to any set limits."""
+
+    def childrenBounds(self, frac=None, orthoRange=(None, None), items=None):
+        bounds = super().childrenBounds(frac=frac, orthoRange=orthoRange, items=items)
+        limits = self.state["limits"]["xLimits"], self.state["limits"]["yLimits"]
+        for i in range(2):
+            if bounds[i] is not None:
+                if limits[i][0] != -1e307:  # and limits[i][0] < bounds[i][0]:
+                    bounds[i][0] = limits[i][0]
+                if limits[i][1] != +1e307:  # and limits[i][1] > bounds[i][1]:
+                    bounds[i][1] = limits[i][1]
+        return bounds
+
+
+class ViewBoxForceScaleAtZero(LimitBoundViewBox):
+    """Viewbox that forces the bottom to be 0."""
+
+    def scaleBy(
+        self,
+        s: list[float] | None = None,
+        center: QtCore.QPointF | None = None,
+        x: float | None = None,
+        y: float | None = None,
+    ) -> None:
+        if center is not None:
+            center.setY(0.0)
+        super().scaleBy(s, center, x, y)
+
+    def translateBy(
+        self,
+        t: QtCore.QPointF | None = None,
+        x: float | None = None,
+        y: float | None = None,
+    ) -> None:
+        if t is not None:
+            t.setY(0.0)
+        if y is not None:
+            y = 0.0
+        super().translateBy(t, x, y)
+
+
+class DGetMSGraph(pyqtgraph.GraphicsView):
+    def __init__(self, parent: QtWidgets.QWidget | None = None):
+        super().__init__(background="white", parent=parent)
+
+        pen = QtGui.QPen(QtCore.Qt.black, 1.0)
+        pen.setCosmetic(True)
+
+        self.xaxis = pyqtgraph.AxisItem("bottom", pen=pen, textPen=pen, tick_pen=pen)
+        self.xaxis.setLabel("m/z")
+
+        self.yaxis = pyqtgraph.AxisItem("left", pen=pen, textPen=pen, tick_pen=pen)
+        self.yaxis.setLabel("Reponse")
+
+        self.plot = pyqtgraph.PlotItem(
+            # title=title,
+            name="central_plot",
+            axisItems={"bottom": self.xaxis, "left": self.yaxis},
+            viewBox=ViewBoxForceScaleAtZero(),
+            parent=parent,
+        )
+        # Common options
+        self.plot.setMenuEnabled(False)
+        self.plot.hideButtons()
+
+        self.ms_series = pyqtgraph.PlotCurveItem(
+            pen=pen, connect="all", skipFiniteCheck=True
+        )
+        self.plot.addItem(self.ms_series)
+
+        self.setCentralWidget(self.plot)
+
+    def drawMSData(self, x: np.ndarray, y: np.ndarray) -> None:
+        self.ms_series.setData(x=x, y=y)
+        self.plot.setLimits(xMin=x.min(), xMax=x.max(), yMin=0.0, yMax=y.max() * 1.2)
+
+
 class DGetMainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent: QtWidgets.QWidget | None = None):
         super().__init__(parent)
@@ -87,24 +167,15 @@ class DGetMainWindow(QtWidgets.QMainWindow):
         self.log = LoggingDialog()
         self.log.setWindowTitle("SPCal Log")
 
+        data = np.loadtxt("/home/tom/Downloads/1.txt")
         self.controls = DGetControls()
         self.results = DGetResults()
-        self.graphics = QtWidgets.QGraphicsView()
-
-        layout = QtWidgets.QHBoxLayout()
-        layout_left = QtWidgets.QFormLayout()
-
-        layout_right = QtWidgets.QVBoxLayout()
-        layout_bottom_bar = QtWidgets.QHBoxLayout()
-        layout_right.addWidget(self.graphics)
-        layout_right.addLayout(layout_bottom_bar, 0)
-
-        layout.addWidget(self.controls, 1)
-        layout.addLayout(layout_right, 1)
+        self.graph = DGetMSGraph()
+        # self.graph.drawMSData(data[:, 0], data[:, 1])
 
         self.addDockWidget(QtCore.Qt.DockWidgetArea.LeftDockWidgetArea, self.controls)
         self.addDockWidget(QtCore.Qt.DockWidgetArea.BottomDockWidgetArea, self.results)
-        self.setCentralWidget(self.graphics)
+        self.setCentralWidget(self.graph)
 
         self.createMenus()
 
