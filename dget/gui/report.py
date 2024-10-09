fe896900311788d9b5f2ab3e4585cbd69335619f
djdt <10266332+djdt@users.noreply.github.com>
2024-10-10 10:10:37 +1100

    report update

--- dget/gui/report.py
+++ dget/gui/report.py
@@ -6,6 +6,8 @@ from dget import __version__
 from dget.dget import DGet
 from dget.gui.graphs import DGetMSGraph
 
+from typing import Sequence
+
 
 def update_cursor_style(
     cursor: QtGui.QTextCursor,
@@ -57,10 +59,10 @@ class DGetReportDialog(QtWidgets.QDialog):
             font_size=8,
             bottom_margin_lines=2,
         )
-        cursor.insertText(f"Report generated using the DGet ({__version__})")
+        cursor.insertText(f"Report generated using the DGet ({__version__}) GUI")
 
     def _addTable(
-        self, cursor: QtGui.QTextCursor, title: str, contents: list[tuple[str, ...]]
+        self, cursor: QtGui.QTextCursor, title: str, contents: Sequence[tuple[str, ...]]
     ) -> QtGui.QTextTable:
         cursor.insertBlock()
         update_cursor_style(
@@ -77,8 +79,6 @@ class DGetReportDialog(QtWidgets.QDialog):
         fm = QtGui.QFontMetrics(cursor.charFormat().font())
 
         table_format = QtGui.QTextTableFormat()
-        # table_format.setBorderStyle(QtGui.QTextFrameFormat.BorderStyle.BorderStyle_None)
-        # table_format.setCellSpacing(fm.averageCharWidth()*2)
         table_format.setWidth(
             QtGui.QTextLength(QtGui.QTextLength.Type.PercentageLength, 50.0)
         )
@@ -117,6 +117,7 @@ class DGetReportDialog(QtWidgets.QDialog):
             ("Adduct", dget.adduct.adduct),
             ("Adduct m/z", f"{dget.adduct.formula.mz:.4f}"),
         ]
+
         cursor = columns.cellAt(0, 0).lastCursorPosition()
         self._addTable(cursor, "Compound Information", cinfo)
         results = [
@@ -127,7 +128,8 @@ class DGetReportDialog(QtWidgets.QDialog):
         probs = probs / probs.sum() * 100.0
         for state, prob in zip(dget.deuteration_states, probs):
             results.append((f"D{state}", f"{prob:.2f} %"))
-        cursor = columns.cellAt(0, 0).lastCursorPosition()
+
+        cursor = columns.cellAt(0, 1).lastCursorPosition()
         self._addTable(cursor, "Results", results)
 
         graph = DGetMSGraph()
@@ -145,64 +147,14 @@ class DGetReportDialog(QtWidgets.QDialog):
 
         self.doc.addResource(self.doc.ResourceType.ImageResource, "ms_graph", pixmap)
 
-        cursor = columns.cellAt(0, 1).lastCursorPosition()
+        cursor.movePosition(cursor.MoveOperation.End, cursor.MoveMode.MoveAnchor)
+        cursor.insertBlock()
+        # update_cursor_style(cursor, align=QtCore.Qt.AlignmentFlag.AlignCenter)
         image_format = QtGui.QTextImageFormat()
-        image_format.setWidth(320)
-        image_format.setHeight(240)
+        image_format.setWidth(640)
+        image_format.setHeight(480)
         image_format.setName("ms_graph")
         cursor.insertImage(image_format, QtGui.QTextFrameFormat.Position.FloatRight)
-
-        # table.cellAt(0, 1).firstCursorPosition().insertText(
-        #     f"{datetime.datetime.now().isoformat(sep=' ', timespec='minutes')}"
-        # )
-        # table.cellAt(1, 0).firstCursorPosition().insertText("Date:")
-
-        # cursor.insertHtml(
-        #     'Please cite <a href="https://doi.org/10.1186/s13321-024-00828-x">'
-        #     "Lockwood and Angeloski (2024)"
-        # )
-
-        #         html = """<!doctype html>
-        # <html>
-        # <body>
-        #     <div class="container">
-        #         <div class="row">
-        #             <h3>Information</h3>
-        #                 <table>
-        #                     <tr><td>Date:</td><td>{{ date }}</td></tr>
-        #                     <tr><td>Notes:</td><td><input type="text"></input></td></tr>
-        #                     <!-- <tr><td>Location:</td><td><input type="text"></input></td></tr> -->
-        #                 </table>
-        #             <h3>Compound Information</h3>
-        #                 <table>
-        #                     <tr><td>Name / ID:</td><td><input type="text"></input></td></tr>
-        #                     <tr><td>Formula:</td><td>{{ formula }}</td></tr>
-        #                     <tr><td>m/z:</td><td>{{ mz|round(4) }}</td></tr>
-        #                     <tr><td>Adduct:</td><td>{{ adduct }}</td></tr>
-        #                     <tr><td>Adduct m/z:</td><td>{{ adduct_mz|round(4) }}</td></tr>
-        #                 </table>
-        #         </div>
-        #         <div class="row">
-        #             <h3>Results</h3>
-        #                 <table>
-        #                     <tr><td>Deuteration:</td><td><b> {{ deuteration|round(2) }} %</td></b></tr>
-        #                     <tr><td>Deuteration Ratio Spectra</td></tr>
-        #                     {% for state, perc in ratios.items() %}
-        #                     <tr><td>D{{ state }}:</td><td>{{ perc|round(2) }} %</td></tr>
-        #                     {% endfor %}
-        #                 </table>
-        #         </div>
-        #         <div class="center">
-        #             <h3>Spectra</h3>
-        #             <img src={{ image }}></img>
-        #         </div>
-        #     </div>
-        #     <p>Report generated using the DGet ({{ version }}) web application ({{ web_version}}).</p>
-        #     <p>Please cite <a href="https://doi.org/10.1186/s13321-024-00828-x">Lockwood and Angeloski (2024)</a>.</p>
-        #     <p>For more information see <a href="https://github.com/djdt/dget">https://github.com/djdt/dget</a>.</p>
-        # </body>
-        # </html>"""
-        #         doc.setHtml(html)
 
     def printReport(self, path: str) -> None:
         printer = QtPrintSupport.QPrinter(
