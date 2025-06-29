518d2b3708f7ec47a00585524c3e94f69ed2ba03
djdt <10266332+djdt@users.noreply.github.com>
2025-06-29 16:57:53 +1000

    add 48px

--- scripts/collect_icons.py
+++ scripts/collect_icons.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--sizes",
         nargs="+",
-        default=[16, 24, 32],
+        default=[16, 24, 32, 48],
         help="icon sizes to use",
     )
     parser.add_argument(
